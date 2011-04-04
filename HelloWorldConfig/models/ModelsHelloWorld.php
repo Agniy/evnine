63248995d6afca922dd96fd9f40ebc330873c02f
Evgeniy Drobov <ev9eniy.freelance@gmail.com>
2011-04-04 22:59:24 +0300

    HelloWorldConfig

---
+++ HelloWorldConfig/models/ModelsHelloWorld.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * HelloWorld
+ * @package HelloWorld
+ * @author ev9eniy
+ * @version 1.0
+ * @created 01-oct-2010 22:03:41
+ */
+class ModelsHelloWorld
+{	 
+ 	function getHelloWorld($param){
+		return $param['hello'];
+	}
+}
+?>
