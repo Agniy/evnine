f2c1dee5660931378a0f33df93f300743681b9d4
Evgeniy Drobov <ev9eniy.freelance@gmail.com>
2011-04-09 18:10:05 +0300

    HelloFrozenDataForDebug

---
+++ HelloFrozenDataForDebug/controllers/ControllersHelloWorld.php
@@ -0,0 +1,34 @@
+<?php
+ /**
+ * HelloWorld
+ * @package Controller
+ * @author *
+ * @version *
+ * @updated *
+ */
+class ControllersHelloWorld
+{
+	var $controller_menu_view;//Array controller
+														//Базовый массив контроллера
+	function __construct($access_level){//Initialize the controller with access levels
+																			//Инициализируем контроллер передавая уровни доступа из конфига
+		$this->controller_menu_view = array(
+			'public_methods' => array(//Public methods are available for all
+					//Example: index.php?t=the controller&m=the public method
+					//Публичные методы доступные всем пользователям
+					//Пример вызова t=имя контроллера&m=публичный метод	
+					'default'=>array(//Default method is always executed unless you specify
+													 //Метод по умолчанию, выполняется всегда если не указан
+						'ModelsHelloWorld' => array(
+							'getContentFromFrozenFile',
+							'getQueryFromTableEvnine',//Call Method from class \models\ModelsHelloWorld->getQuery
+							'setUpdateTableEvnineModifierParamSetResetFalse',
+							'getQueryFromTableEvnineAfterUpdate'
+						), 
+					),
+			)
+		);
+	}
+} 
+
+?>
