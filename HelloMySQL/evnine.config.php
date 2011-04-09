9b96e15214432c91f3e46d528ab3e6a52c09a81e
Evgeniy Drobov <ev9eniy.freelance@gmail.com>
2011-04-09 11:00:18 +0300

    HelloMySQL

---
+++ HelloMySQL/evnine.config.php
@@ -0,0 +1,43 @@
+<?php
+class Config 
+{
+
+	var $param_const;
+	var $path_to;
+	var $access_level;
+	var $class_path;
+	var $controller_menu_view;
+
+	function __construct(){
+
+		$this->path_to='';
+		$this->api='ModelsMySQL';
+		$this->access_level=array(
+			'guest'=>'0',
+		);
+
+		$this->param_const=array(
+		);
+		$this->controller_menu_view=array(
+			'helloworld'=>'ControllersHelloWorld',
+		);
+		$this->class_path=array(
+			'ModelsMySQL'=>array(
+				'path'=>'models'.DIRECTORY_SEPARATOR,
+				'param'=>array(
+					'host'=>'localhost',
+					'login' => 'root',
+					'pass' => 'root',
+					'db' => 'evnine'
+				),
+			),
+			'ModelsHelloWorld'=>array(
+				'path'=>'models'.DIRECTORY_SEPARATOR,
+				'param'=>array(
+					'hello'=>'config',
+				),
+			),
+		);	
+	}
+}
+?>
---
+++ HelloMySQL/models/ModelsHelloWorld.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * HelloWorld
+ * @package HelloWorld
+ * @author ev9eniy
+ * @version 1.0
+ * @created 04-apr-2010 11:03:41
+ */
+class ModelsHelloWorld
+{	
+
+	var $api;
+
+	/**
+	 * Constructor
+	 */
+	function __construct($api){
+		$this->api=$api;//save api (MySQL link to class)
+	}
+
+	function getQuery($param) {
+		$array=array();
+		$array['databases']=$this->api->getQuery('show databases');
+		$array['query_error']=$this->api->getQuery('fasdfas');
+		return $array;
+	}
+}
+?>
