7322deb38671b27b5fdfe39cecab54d8372ba593
Evgeniy Drobov <ev9eniy.freelance@gmail.com>
2011-04-10 10:34:35 +0300

    HelloFormMultiMethodSumbit

---
+++ HelloFormMultiMethodSumbit/index.php
@@ -0,0 +1,61 @@
+<?php
+error_reporting(2046);
+include_once 'evnine.php';
+include_once 'debug/evnine.debug.php';
+
+$evnine = new Controller();
+$ctrlr = $evnine->getControllerForParam(
+	array(
+		'controller' => 'validation',
+		'form_data' => $_REQUEST,
+		'ajax' => 'ajax',
+	)
+);
+
+
+?>
+
+<form 
+	method="get" 
+	action=<?php echo $ctrlr['inURL']['default']['pre'].$ctrlr['inURL']['post']; ?>
+>
+<?php echo $ctrlr['inURL']['default']['inputs']; ?>
+<input type="text" 
+	value="<?php echo $ctrlr['REQUEST_OUT']['PathID']; ?>" 
+	name="<?php echo $ctrlr['inURL']['default']['PathID'] ?>"
+/>
+<input 
+	type="submit"
+	name="<?php echo $ctrlr['inURL']['default']['submit'] ?>"
+	value="submit default"
+/>
+
+<input 
+	type="submit"
+	name="<?php echo $ctrlr['inURL']['submit_1']['submit'] ?>"
+	value="Method: submit_1"
+/>
+<input 
+	type="submit"
+	name="<?php echo $ctrlr['inURL']['submit_2']['submit'] ?>"
+	value="Method: submit_2"
+/>
+<input 
+	type="submit"
+	name="<?php echo $ctrlr['inURL']['submit_3']['submit'] ?>"
+	value="Method: submit_3"
+/>
+<input 
+	type="submit"
+	name="<?php echo $ctrlr['inURL']['submit_4']['submit'] ?>"
+	value="Method: submit_4"
+/>
+</form>
+
+
+<?php
+
+print_r2($ctrlr, "array",false);
+
+?>
+
