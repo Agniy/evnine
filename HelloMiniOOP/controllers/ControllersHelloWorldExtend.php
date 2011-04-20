51b479ec3559e03b692caf4d95c94394d4915152
Evgeniy Drobov <ev9eniy.freelance@gmail.com>
2011-04-20 22:19:43 +0300

    HelloMiniOOP

---
+++ HelloMiniOOP/controllers/ControllersHelloWorldExtend.php
@@ -0,0 +1,63 @@
+<?php
+ /**
+ * HelloWorld
+ * @package Controller
+ * @author *
+ * @version *
+ * @updated *
+ */
+class ControllersHelloWorldExtend
+{
+	var $controller_menu_view;//Array controller
+														//Базовый массив контроллера
+	function __construct($access_level){//Initialize the controller with access levels
+																			//Инициализируем контроллер передавая уровни доступа из конфига
+		$this->controller_menu_view = array(
+			'private_methods' => array(
+				//Private methods are not available for outside calls.
+				//available only when a call from the public methods.
+				#
+				/*Приватные методы не доступные для вызова из вне,*/
+				/*доступны только при вызова из публичных методов*/
+				#
+				'isGetNotHelloWorld_true'=>array(
+					'ModelsHelloWorld' => 'getNotHelloWorld',
+				),
+				'isGetNotHelloWorld_false'=>array(
+					'ModelsHelloWorld' => 'getNotHelloWorld',
+				)
+				//'isHasAccess'=>array(
+					//Check for access?
+					#
+					/*Проверить, а есть ли доступ?*/
+					//For example, for the validation check cookies
+					#
+					/*Например для валидации проверяем куки*/
+					#
+					//'ModelsValidation'  => 'isValidCookie',
+					//),
+				),
+				
+				'public_methods' => array(//Public methods are available for all
+
+					//Example: index.php?t=the controller&m=the public method
+					//Публичные методы доступные всем пользователям
+					//Пример вызова t=имя контроллера&m=публичный метод	
+					'default'=>array(//Default method is always executed unless you specify
+													 //Метод по умолчанию, выполняется всегда если не указан
+//						'ModelsHelloWorld' => array(
+//							'isGetNotHelloWorld',
+//						), //Call Method from class \models\ModelsHelloWorld->isGetNotHelloWorld
+							// If answer is true 
+							// Call private method from controller::callPrivateMethod1
+							#
+							/*Если ответ правда */ 
+							/* Вызываем приватный метод в контроллере */
+							
+					),
+			)
+		);
+	}
+} 
+
+?>
---
+++ HelloMiniOOP/evnine.config.php
@@ -0,0 +1,35 @@
+<?php
+class Config 
+{
+
+var $param_const;
+var $path_to;
+var $access_level;
+var $class_path;
+var $controller_menu_view;
+
+	function __construct(){
+
+	$this->path_to='';
+	$this->access_level=array(
+		'guest'=>'0',
+	);
+
+	$this->param_const=array(
+		'default_controller'=>'default_controller',
+	);
+	$this->controller_menu_view=array(
+		'helloworld'=>'ControllersHelloWorld',
+		'helloworldext'=>'ControllersHelloWorldExtend',
+	);
+	$this->class_path=array(
+		'ModelsHelloWorld'=>array(
+			'path'=>'models'.DIRECTORY_SEPARATOR,
+			),
+		'ControllersHelloWorldExtend'=>array(
+			'path'=>'models'.DIRECTORY_SEPARATOR,
+		),
+	);	
+	}
+}
+?>
