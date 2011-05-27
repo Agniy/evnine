d341469f2a890c064ed87c49db6da08609feb19c
Evgeniy Drobov <ev9eniy.freelance@gmail.com>
2011-05-27 22:27:14 +0300

    HelloTemplates

---
+++ HelloTemplates/evnine.config.php
@@ -0,0 +1,32 @@
+<?php
+class Config 
+{
+
+var $param_const;
+var $path_to;
+var $access_level;
+var $class_path;
+var $controller_menu_view;
+
+	function __construct(){
+
+	$this->path_to='';
+	$this->access_level=array(
+		'guest'=>'0',
+	);
+
+	$this->param_const=array(
+		'default_controller'=>'default_controller',
+	);
+	$this->controller_menu_view=array(
+		'helloworld'=>'ControllersHelloWorld',
+		'helloworld2'=>'ControllersHelloWorld2',
+	);
+	$this->class_path=array(
+		'ModelsHelloWorld'=>array(
+			'path'=>'models'.DIRECTORY_SEPARATOR,
+			),
+	);	
+	}
+}
+?>
