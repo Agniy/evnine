7b722a2c971e3c9778e43191bb9b199df454b48f
Evgeniy Drobov <ev9eniy.freelance@gmail.com>
2011-05-29 16:17:29 +0300

    Added

---
+++ HelloExceptionThrowError/controllers/ControllersHelloWorld.php
@@ -0,0 +1,39 @@
+<?php
+ /**
+ * HelloWorld
+ * @package Controller
+ * @author *
+ * @version *
+ * @updated *
+ */
+class ControllersHelloWorld
+{
+	var $controller_menu_view;
+	// en: Array controller
+	/* ru: Базовый массив контроллера*/
+	function __construct($access_level){
+	// en: Initialize the controller with access levels
+	/* ru: Инициализируем контроллер передавая уровни доступа из конфига*/
+		$this->controller_menu_view = array(
+			'public_methods' => array(
+					//Public methods are available for all
+					//Example: index.php?t=the controller&m=the public method
+					
+					/*Публичные методы доступные всем пользователям*/
+					/*Пример вызова t=имя контроллера&m=публичный метод	*/
+				
+					'default'=>array(
+						'ModelsHelloWorld' => array(
+							'setThrowNewException'
+						),
+						'ModelsErrors'=>array(
+							'getError',
+							'getError->alternative_way_of_setting_errors',
+						)
+					),
+			),
+		);
+	}
+} 
+
+?>
