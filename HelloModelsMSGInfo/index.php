85ce92f26bbbc4ce2a9082080676e7abb28f7577
Evgeniy Drobov <ev9eniy.freelance@gmail.com>
2011-05-29 18:42:56 +0300

    Added models msg

---
+++ HelloModelsMSGInfo/index.php
@@ -0,0 +1,18 @@
+<?php
+error_reporting(0);
+include_once 'evnine.php';
+include_once 'debug/evnine.debug.php';
+include_once 'evnine.views.generator.template.php';
+
+$evnine = new Controller();
+$ctrlr = $evnine->getControllerForParam(
+	array(
+		'controller' => 'hello',
+		'ajax' => 'ajax',
+	)
+);
+
+print_r2($ctrlr, "array",false);
+
+
+?>
