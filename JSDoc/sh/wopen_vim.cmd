af654ea77d371555e5f60adabdbfb4f9e8123377
Evgeniy Drobov <ev9eniy.freelance@gmail.com>
2011-06-19 22:26:53 +0300

    update scripts

---
+++ JSDoc/sh/wopen_vim.cmd
@@ -0,0 +1,9 @@
+if not -%1==- start CMD /C VIM %1
+if not -%2==- start CMD /C VIM %2
+if not -%3==- start CMD /C VIM %3
+if not -%4==- start CMD /C VIM %4
+if not -%5==- start CMD /C VIM %5
+if not -%6==- start CMD /C VIM %6
+if not -%7==- start CMD /C VIM %7
+if not -%8==- start CMD /C VIM %8
+if not -%9==- start CMD /C VIM %9
