e85a2047f5ae4bd25ef35e982c8a8946da8d0dd7
Evgeniy Drobov <ev9eniy.freelance@gmail.com>
2011-08-23 22:05:34 +0300

    Lesson 3

--- HelloCMSBitrixAPI/bitrix/components/evnine/evnine/templates/evnine/result_debug.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo '#$arResult: <pre>'; if(function_exists(print_r2)) print_r2($arResult); else print_r($arResult);echo "</pre><br />\r\n";
-?>
--- HelloCMSBitrixAPITwig/bitrix/components/evnine/evnine/templates/evnine/result_debug.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo '#$arResult: <pre>'; if(function_exists(print_r2)) print_r2($arResult); else print_r($arResult);echo "</pre><br />\r\n";
-require_once($_SERVER["DOCUMENT_ROOT"].'/bitrix/modules/evnine/classes/general/debug/evnine.views.generator.template.php');
-echo $twig_generator= 
-	getTemplateFromArray(
-		$arResult,
-		$template_param1= array('echo' => true,'if' => false,'comment' => true,'tpl' => 'twig')
-	);
-?>
